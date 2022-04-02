a7bb30bd7c8557b954dce9e659b99628fcd0ee7b
garrettkatz <garrett.katz@gmail.com>
2022-04-02 11:55:03 -0400

    early branching in fit_lr_nd, staging N3M4

--- fit_lr_nd.py
+++ fit_lr_nd.py
@@ -3,13 +3,18 @@ import itertools as it
 import numpy as np
 import matplotlib.pyplot as pt
 import scipy.optimize as so
-from nondet_unsized import NonDeterminator
+# from nondet_unsized import NonDeterminator
+from nondet_sized import NonDeterminator
 
-rerun = False
+rerun = True
 
-N = 2 # number of neurons
-M = 2 # number of key-value pairs
-save_depth = 0
+# N = 2 # number of neurons
+# M = 2 # number of key-value pairs
+# save_depth = 0
+
+N = 3 # number of neurons
+M = 4 # number of key-value pairs
+save_depth = 1
 
 fname = f"hemis_{N}.npy"
 with open(fname,"rb") as f: weights, H = pk.load(f)
@@ -79,11 +84,65 @@ def checkfit():
 
     return True, θ, W
 
+def checkfit_progressive():
+    # queue root net with identity weights
+    m1 = [np.isclose(np.eye(N)[i], weights).all(axis=1).argmax() for i in range(N)]
+    m2 = list(m1)
+    W = {kidx: np.stack((np.eye(N),)*2)}
+    queue = [kidx]
+
+    # init term data
+    terms = [np.empty((E, T0, N, N)), np.empty((E, T1, N, N))]
+    targ_vidxs = []
+    e = 0
+    while len(queue) > 0:
+        # choose next node to fit
+        vidx = queue.pop()
+
+        # enumerate edges
+        for (j,k) in it.product(range(M), repeat=2):
+            terms[0][e], terms[1][e] = lrterms_fser(W[vidx], V[:,kidx[j]], V[:,kidx[k]])
+            new_vidx = vidx[:j] + (kidx[k],) + vidx[j+1:]
+            targ_vidxs.append(new_vidx)
+            
+            # handle new nodes
+            if new_vidx not in W:
+                # choose net for new node
+                hidx, m1, m2 = nd.choice(solns[new_vidx])
+                W[new_vidx] = np.empty((2,N,N))
+                for l,mm in enumerate((m1, m2)):
+                    for i in range(N):
+                        mi = nd.choice(mm[i])
+                        W[new_vidx][l,i] = weights[mi]
+                
+                # add to queue
+                queue.append(new_vidx)
+
+            # advance edge counter
+            e += 1
+
+            # resolve theta
+            θ = [np.empty((T0, N, N)), np.empty((T1, N, N))]
+            for l in range(2):
+                for i,j in it.product(range(N), repeat=2):
+                    A = terms[l][:e,:,i,j]
+                    b = np.array([W[vidx][l,i,j] for vidx in targ_vidxs[:e]])
+                    x = np.linalg.lstsq(A, b, rcond=None)[0]
+        
+                    if np.fabs(b - (A @ x)).max() > .4: return False, θ, W
+        
+                    θ[l][:,i,j] = x
+
+    return True, θ, W
+
 if rerun:
 
-    for r, (success, θ, W) in enumerate(nd.runs(checkfit)):
-    
-        print(f"run {r}")
+    # for r, (success, θ, W) in enumerate(nd.runs(checkfit)):
+    for r, (success, θ, W) in enumerate(nd.runs(checkfit_progressive)):
+
+        
+        ctr = " ".join(f"{co}/{len(ch)}" for co,ch in zip(nd.counters, nd.choices) if len(ch) > 1)
+        print(f"run {r}: {ctr}")
         if success:
             with open(f"fitlr_{N}_{M}.pkl", "wb") as f: pk.dump((θ, W), f)
 
@@ -115,112 +174,3 @@ for t in range(100):
 
 print("Checked out")
 
-# # LR(W, x, y):
-# # lrterms(W, x, y) -> x, W1x, sign(W2 sign(W1x)), etc
-# # make each term shape (N,N) (outer products, broadcast, etc)
-# # W_new[i,j] <- (stack(lrterms(W, x, y))[:,i,j] * theta[:,i,j]).sum(axis=0)
-# # W_new[i] @ x = ((stack(lrterms(W, x, y))[:,i,:] * theta[:,i,:]).sum(axis=0) * x).sum()
-
-# # e is edges in graph
-# # for all e,i,j:
-# # - out[e,i,j] * (W_new[e,i,k] * inp[e,k,j]).sum(k) <= -1
-# # - out[e,i,j] * ((theta[e,t,i,k] * terms[e,t,i,k]).sum(t) * inp[e,k,j]).sum(k) <= -1
-# # (- out[e,i,j] * inp[e,k,j] * terms[e,t,i,k] * theta[t,i,k]).sum(t,k) <= -1
-# # (- out[e,i,j] * inp[e,k,j] * terms[e,i,t,k] * theta[i,t,k]).sum(t,k) <= -1
-
-# # (- out[i,e,j,0,0] * inp[0,e,j,0,k] * terms[i,e,0,t,k] * theta[i,0,0,t,k]).sum(t,k) <= -1
-
-# def fitlr(terms, inp, out):
-
-#     # out[e,i,j] -> [i,e,j,0,0]
-#     out = out.transpose(axes=(1,0,2))[:,:,:,np.newaxis,np.newaxis]
-#     # inp[e,k,j] -> inp[0,e,j,0,k]
-#     inp = inp.transpose(axes=(0,2,1))[np.newaxis,:,:,np.newaxis,:]
-#     # terms[e,t,i,k] -> terms[i,e,0,t,k]
-#     terms = terms.transpose(axes=(2,0,1,3))[:,:,np.newaxis,:,:] # put t after i
-
-#     T, K = terms.shape[3:]
-    
-#     θ = []
-#     for i in range(inp.shape[0]):
-
-#         A_ub = -out * inp * terms
-#         A_ub = A_ub.reshape(-1, T*K)
-#         b_ub = -np.ones(A_ub.shape[0])
-#         c = -A_ub.mean(axis=0)
-#         result = so.linprog(c, A_ub, b_ub, bounds=(None, None))#, method='simplex')
-
-#         if not result.status in (0, 3): return False, θ
-#         θ.append(result.x.reshape((T,K)))
-
-#     # theta[i,t,k] -> theta[t,i,k]
-#     θ = np.stack(θ).tranpose(axes=(1,0,2))
-#     return True, θ
-
-# T_fser = 3
-# def lrterms_fser(W, x, y):
-#     z = np.sign(W @ x)
-#     terms = np.stack((W, y * x.T, z * x.T))
-#     return terms
-
-# N = 2 # number of neurons
-# M = 2 # number of key-value pairs
-
-# V = np.array(tuple(it.product((-1, 1), repeat=N))).T
-
-# w1 = np.eye(N)
-# w2 = np.eye(N)
-# # kidx = tuple(range(M))
-# kidx = {
-#     2: (0, 1),
-#     4: (0, 3, 5, 6)
-# }[N]
-
-# print(V)
-# print(V[:,kidx])
-
-# nd = NonDeterminator()
-
-
-# E = M**(M+2)
-# print(f"{E} edges...")
-# input('.')
-
-# def checkfit():
-
-#     # out[e,i,j] -> [i,e,j,0,0]
-#     # inp[e,k,j] -> inp[0,e,j,0,k]
-#     # terms[e,t,i,k] -> terms[i,e,0,t,k]
-#     # 2*E for two layers
-#     inp = np.zeros((2*E, N, M))
-#     out = np.zeros((2*E, N, M))
-#     terms = np.zeros((2*E, T_fser, N, N))
-
-#     # identity net vidx = hidx = kidx
-#     hidxs = {kidx: kidx}
-#     weights = {kidx: (np.eye(N), np.eye(N))}
-#     queue = [kidx]
-    
-#     e = 0
-#     while len(queue) > 0:
-#         vidx = queue.pop()
-#         hidx = hidxs[vidx]
-
-#         for (j, k) in it.product(range(M), repeat=2):
-#             new_vidx = vidx[:j] + (kidx[k],) + vidx[j+1:]
-#             if new_vidx not in hidxs:
-#                 # hidxs[new_vidx] = np.choice(it.product(range(2**N), repeat=M))
-#                 hidxs[new_vidx] = kidx # first layer identity
-#             new_hidx = hidxs[new_vidx]
-
-#             # # layer 1
-#             # inp[e] = V[:, kidx]
-#             # out[e] = V[:, new_hidx]
-#             # terms[e] = lrterms_fser(weights[vidx][0], V[:,kidx[j]], ?])
-#             # e += 1
-
-#             # layer 2
-#             inp[e] = V[:, new_hidx]
-#             out[e] = V[:, new_vidx]
-#             terms[e] = lrterms_fser(weights[vidx][1], V[:,new_hidx[j]], V[:,new_vidx[j]]])
-#             e += 1
