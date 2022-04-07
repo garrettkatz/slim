ffa4fd9600958ad9b370547a236540f086cfc567
garrettkatz <garrett.katz@gmail.com>
2022-04-07 19:12:20 -0400

    fixed count perms

--- count_perms.py
+++ count_perms.py
@@ -1,22 +1,20 @@
 import itertools as it
 import numpy as np
 
-N = 2
+N = 3
 V = np.array(tuple(it.product((-1, 1), repeat=N))).T
 
-# this is wrong because W rows/cols are not one-hot
+P = []
 
-P = set()
-for entries in it.product((-1, 0, 1), repeat=N**2):
-    W = np.array(entries).reshape((N, N))
-    WV = W @ V
-    p = tuple(
-        (WV[:,j:j+1] == V).all(axis=0).argmax()
-        for j in range(2**N))
-    # p = np.empty(2**N, dtype=int)
-    # for j in range(2**N):
-    #     p[j] = (WV[:,j:j+1] == V).all(axis=0).argmax()
-    P.add(p)
+for perm in it.permutations(range(N)):
+    for signs in it.product((-1, 1), repeat=N):
+        WV = (np.array(signs).reshape(-1,1) * V)[perm, :]
+        p = tuple(
+            (WV[:,j:j+1] == V).all(axis=0).argmax()
+            for j in range(2**N))
+        P.append(p)
 
+print(2**N * np.arange(1,N+1).prod())
 print(len(P))
 
+print(np.array(P))
