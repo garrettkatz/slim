8378e208bd128f4f20f59082aeb677f78fe8d64f
garrettkatz <garrett.katz@gmail.com>
2022-06-05 00:49:56 -0400

    some interesting new muroga checks

--- muroga_check.py
+++ muroga_check.py
@@ -12,16 +12,23 @@ X = np.array(tuple(it.product((-1, 1), repeat=9))).T
 # check multiple extrema of Muroga 1970
 wa = np.array([13, 7, 6, 6, 4, 4, 4, 3, 2])
 wb = np.array([13, 7, 6, 6, 4, 4, 4, 2, 3])
+wc = np.array([13, 7, 6, 6, 4, 4, 4, 2.5, 2.5])
 ya = np.sign(wa @ X)
 yb = np.sign(wb @ X)
+yc = np.sign(wc @ X)
 print((ya == yb).all())
+print((ya == yc).all())
+
+print("wa|wb, wc norms:")
+print((wb**2).sum())
+print((wc**2).sum())
 
 # Sanity check by linprog
 A = X[:, ya < 0].T
 b = -np.ones(2**8)
 c = np.ones(9)
 result = linprog(c, A_ub=A, b_ub=b, bounds=(None, None))
-print(result.x)
+wlp = result.x
 
 # Linprog the boundaries
 Xh = X[:, ya < 0].T
@@ -41,10 +48,24 @@ for b in range(2**8):
     boundaries[b] = (Xh[others] @ w <= -.99).all() and np.fabs(Xh[b] @ w) <= 0.01
     print(f"{b} of {2**8}, {boundaries.sum()} boundaries")
 
+print("lp'd w:")
+print(wlp)
+
 w = np.linalg.lstsq(Xh[boundaries], np.ones(boundaries.sum()), rcond=None)[0]
+print("lstsq'd w, dot with boundaries, max residual:")
 print(w)
 print(Xh[boundaries] @ w)
 print(np.fabs((Xh[boundaries] @ w) - np.ones(boundaries.sum())).max())
 
+print("wa, wb, wc dots with boundaries:")
+print(Xh[boundaries] @ wa)
+print(Xh[boundaries] @ wb)
+print(Xh[boundaries] @ wc)
+print("wa, wb, wc min abs dots with non-boundaries:")
+print(np.fabs(Xh[~boundaries] @ wa).min())
+print(np.fabs(Xh[~boundaries] @ wb).min())
+print(np.fabs(Xh[~boundaries] @ wc).min())
+
 # looks like w @ Xb = 1 does not generalize past N=7.
+# however, perhaps w @ Xb <= 2, w @ X~b >= 3? maybe minimum L2 norm w?
 
