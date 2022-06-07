0aaf9f29d9f3546d132026a51db8083a18eb114b
garrettkatz <garrett.katz@gmail.com>
2022-06-06 22:50:50 -0400

    rayshoot tests/plots

--- rayshoot.py
+++ rayshoot.py
@@ -20,25 +20,48 @@ def rayshoot(u, v):
     return n_opt, d_opt, x_opt, crossings
     
 if __name__ == "__main__":
-
-    N = 5
-    X = np.array(tuple(it.product((-1, +1), repeat=N)))
-
-    u = np.random.randint(-3, 4, size=N)
-    v = np.random.choice((-1, 1), size=N)
-
-    n, d, x, c = rayshoot(u, v)
-    print(x)
-    print(n, d, n/d)
-
-    Xu, Xv = X @ u, X @ v
-    alpha = Xu / Xv
-    keep = (Xv != 0) & (alpha >= 0)
-    mn = alpha[keep].min()
-    print(mn)
-    print(X[keep][alpha[keep] == mn])
-
-    print((X[keep][alpha[keep] == mn] == x).all(axis=1))
     
-    
+    Ns = np.arange(2, 18)
+    csizes = {}
+    for N in Ns:
+        print(f"{N}...")
+        X = np.array(tuple(it.product((-1, +1), repeat=N)))
 
+        csizes[N] = []
+        for r in range(30):
+    
+            # u = np.random.randint(-2*N, 2*N+1, size=N)
+            u = np.random.randint(-N, N+1, size=N)
+            v = np.random.choice((-1, 1), size=N)
+        
+            n, d, x, c = rayshoot(u, v)
+            csizes[N].append(sum(map(len, c.values())))
+            
+            # print(x, n, d)
+            # if n != None: print(n/d)
+        
+            Xu, Xv = X @ u, X @ v
+            alpha = Xu / Xv
+            keep = (Xv != 0) & (alpha >= 0)
+
+            if not keep.any():
+                assert n == None
+
+            else:
+
+                ak = alpha[keep]
+                mn = ak.min()
+                Xm = X[keep][ak == mn]
+                # print(mn)
+                # print(Xm)
+                # print((Xm == x).all(axis=1))
+
+                assert (Xm == x).all(axis=1).any()
+    
+    import matplotlib.pyplot as pt
+    pt.plot(Ns, 2**Ns, 'k--')
+    for N, csz in csizes.items():
+        pt.plot([N]*len(csz), csz, '.', color=(.5,)*3)
+    pt.yscale("log")
+    pt.show()
+        
