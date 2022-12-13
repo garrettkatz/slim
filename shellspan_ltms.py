99e9b5c6eaed1799314d43c2651a79f0fd0fb287
garrettkatz <garrett.katz@gmail.com>
2022-12-12 23:44:49 -0500

    some shellspans still fail

--- shellspan_ltms.py
+++ shellspan_ltms.py
@@ -16,19 +16,27 @@ if __name__ == "__main__":
     # print(sX)
     # print(np.concatenate((Y, dist[:,np.newaxis], W.round().astype(int)), axis=1))
 
-    A = adjacency(Y)
+    A = adjacency(Y, sym=True)
 
     # For identity dichotomies (Y is a row of X) use identity weights (seem linprog already does this)
     fit = (dist == 0)
 
-    # Fit remaining dichotomies in order of shellone after another, maintaining span constraints with all adjacent previous fits
-    for i in range(1,len(A)):
-        print(f"ltm {i} of {len(A)}")
+    # # Only fit the farthest from identity dichotomies
+    # fit = (dist < udist.max())
+
+    # Fit remaining dichotomies, maintaining span constraints with all adjacent previous fits
+    unfit = np.flatnonzero(~fit)
+    for z,i in enumerate(unfit):
+        print(f"far ltm {z} (d={dist[i]}) of {len(unfit)}")
+
+        # dichotomy to fit
+        y = Y[i]
 
         # construct null-spaces for each already-fit neighbor span
         A_null = []
         for j in A[i]:
-            if j > i: break # omit neighbors that have not been fit yet
+            # all d==2 neighbors are in d==1, should be already fit
+            if not fit[j]: continue # omit neighbors that have not been fit yet
 
             # column where neighbors differ
             k = (Y[i] != Y[j]).argmax()
@@ -37,26 +45,42 @@ if __name__ == "__main__":
             A_null.append( null_space(np.stack((W[j], X[:,k]))) ) # (N, N-2)
 
         # Big (redundant) null space basis over all neighbors
-        A_null = np.concatenate(A_null, axis=1)
+        if len(A_null) > 0:
 
-        # Fit dichotomy and span constraints simultaneously
-        y = Y[i]
-        result = linprog(
-            c = (X * y).sum(axis=1),
-            A_ub = -(X * y).T,
-            b_ub = -np.ones(len(y)),
-            A_eq = A_null.T,
-            b_eq = np.zeros(A_null.shape[1]),
-            bounds = (None, None),
-        )
+            # Fit dichotomy and span constraints simultaneously
+            A_null = np.concatenate(A_null, axis=1)
+            result = linprog(
+                c = (X * y).sum(axis=1),
+                A_ub = -(X * y).T,
+                b_ub = -np.ones(len(y)),
+                A_eq = A_null.T,
+                b_eq = np.zeros(A_null.shape[1]),
+                bounds = (None, None),
+            )
+
+        else:
+
+            # No span constraints yet
+            result = linprog(
+                c = (X * y).sum(axis=1),
+                A_ub = -(X * y).T,
+                b_ub = -np.ones(len(y)),
+                bounds = (None, None),
+            )
+
         W[i] = result.x
+        fit[i] = True
         feasible = (np.sign(W[i] @ X) == y).all()
 
         if not feasible:
-            print(f"failure at ltm {i}")
+            print(f"failure at ltm {z}")
             break
 
+    if feasible:
 
+        assert (np.sign(W @ X) == Y).all()
+        print(W)
+        print("it worked??")
 
 
 
