042c339cde2acc0ad50628e01c55085d2999e5f5
garrettkatz <garrett.katz@gmail.com>
2023-11-16 21:38:31 -0500

    graph fitness + geneng success for Ns 3-5

---
+++ geneng_graph.py
@@ -0,0 +1,112 @@
+import pickle as pk
+from geneng_array import *
+from graph_fitness import load_ltm_data, organize_by_source, graph_fitness
+
+if __name__ == "__main__":
+
+    Ns = [3,4,5,6]
+    do_span = True # whether to constrain learning rule to be span rule
+
+    do_opt = True # whether to run optimization
+    do_check = True # whether to check result of optimization
+
+    # ops imported from geneng_ab
+    productions = [Constant, Dimension, Variable, Add, Sub, Mul, Div, Power, Maximum, Minimum, Dot, Sign, Sqrt, Log2, Sum, Min, Max]
+    
+    if do_span:
+        grammar = extract_grammar(productions, SpanRule)
+    else:
+        grammar = extract_grammar(productions, VecRule)
+
+    print("Grammar: {}.".format(repr(grammar)))
+
+    Yc, W, X, Ac = load_ltm_data(Ns)
+    W0 = {N: W[N][0] for N in Ns}
+
+    def learning_rule_factory(n: Array):
+
+        def learning_rule(w, x, y):
+            # geneng_array line of dataset expects 2D arrays, one row per example, but graph_fitness inputs 1D w and x and scalar y
+            line = [w.reshape(1,-1), x.reshape(1,-1), np.array([[y]])]
+            w_new = n.eval(line)[0]
+            return w_new
+
+        return learning_rule
+
+    def fit_fun(n: Array):
+
+        learning_rule = learning_rule_factory(n)
+        region_loss, match_loss, W = graph_fitness(learning_rule, W0, X, Yc, Ac, eps=.1, verbose=False)
+        # print(region_loss)
+
+        # region loss should take precedence, but find a better way to combine with match_loss (or multiobjective)
+        total_loss = region_loss + 0.1 * match_loss
+
+        # NaNs might mess up genetic engine, convert to large finite loss
+        if not np.isfinite(total_loss): total_loss = 10**6
+
+        return total_loss
+    
+    prob = SingleObjectiveProblem(
+        minimize=True, # smaller loss is better
+        fitness_function=fit_fun,
+    )
+
+    if do_opt:
+        alg = SimpleGP(
+            grammar,
+            problem=prob,
+            # probability_crossover=0.4,
+            # probability_mutation=0.4,
+            number_of_generations=1000,
+            max_depth=7,
+            population_size=2000,
+            selection_method=("tournament", 2),
+            n_elites=200,
+            n_novelties=400,
+            seed = np.random.randint(123456), # 123 for reproducible convergence on perceptron
+            target_fitness=0.0,
+            # favor_less_complex_trees=True,
+        )
+
+        best = alg.evolve()
+
+
+        # fitness, formula = best.get_fitness(prob), best.genotype
+        fitness, formula = best.get_fitness(prob), best.genotype
+
+    #     with open(f"graph_best_{'_'.join(map(str, Ns))}.pkl", "wb") as f: pk.dump((fitness, formula), f)
+
+    # with open(f"graph_best_{'_'.join(map(str, Ns))}.pkl", "rb") as f: fitness, formula = pk.load(f)
+
+    print(
+        f"Fitness of {fitness} by genotype: {formula}",
+    )
+
+    if do_check:
+
+        learning_rule = learning_rule_factory(formula)
+        region_loss, match_loss, W = graph_fitness(learning_rule, W0, X, Yc, Ac, eps=.1, verbose=False)
+        print(f"region loss = {region_loss}, match loss = {match_loss}")
+        for N in Ns:
+            print(f"N={N}, W[N]:")
+            print(W[N])
+
+            A = organize_by_source(Ac[N])
+
+            # run a bunch of random transitions
+            T = 100
+            i = np.random.randint(W[N].shape[0])
+            w = W[N][i]
+            for t in range(T):
+
+                # check current region
+                assert ((w * (X[N] * Yc[N][i]).T).sum(axis=1) > 0).all()
+
+                # transition to new region
+                j, k = A[i][np.random.randint(len(A[i]))]
+                w = learning_rule(w, X[N][:,k], Yc[N][j,k])
+                i = j
+
+        print("check passed.")
+
--- graph_fitness.py
+++ graph_fitness.py
@@ -1,4 +1,28 @@
+import pickle as pk
 import numpy as np
+
+def load_ltm_data(Ns):
+    Yc, W, X, Ac = {}, {}, {}, {}
+
+    for N in Ns:
+
+        # load canonical hemis
+        ltms = np.load(f"ltms_{N}_c.npz")
+        Yc[N], W[N], X[N] = ltms["Y"], ltms["W"], ltms["X"]
+
+        with open(f"adjs_{N}_jc.npz", "rb") as f: Ac[N] = pk.load(f)
+
+    return Yc, W, X, Ac
+
+# organize adjacencies by source region
+# Ac_N: Ac[N] for one N
+# returns A_N[i] = [..., (j,k), ...] adjacent region j with boundary k to region i
+def organize_by_source(Ac_N):
+    A = {}
+    for (i,j,k) in Ac_N:
+        if i not in A: A[i] = []
+        A[i].append((j, k))
+    return A
 
 """
 Runs a graph search over the canonical region adjacency graphs
@@ -18,9 +42,11 @@ def graph_fitness(learning_rule, w0, X, Yc, Ac, eps=1, verbose=False):
 
     # loss when the rule violates a region constraint
     region_loss = 0.
+    region_count = 0 # denominator for average
 
     # loss when the rule produces a different weight vector when transitioning to a previously explored region
     match_loss = 0.
+    match_count = 0 # denominator for average
 
     # Saves resulting weights produced by learning rule
     W = {}
@@ -29,16 +55,13 @@ def graph_fitness(learning_rule, w0, X, Yc, Ac, eps=1, verbose=False):
     for N in X.keys():
 
         # organize adjacencies by source region
-        A = {}
-        for (i,j,k) in Ac[N]:
-            if i not in A: A[i] = []
-            A[i].append((j, k))
+        A = organize_by_source(Ac[N])
 
         if verbose: print(f"{N} starting, {len(A)} regions")
 
         # init graph search
-        explored = {}
-        queue = [(0, w0[N])]
+        explored = {} # explored[i] = w_i
+        queue = [(0, w0[N])] # (dichotomy index i, w_i)
 
         # run graph search
         while len(queue) > 0:
@@ -54,6 +77,7 @@ def graph_fitness(learning_rule, w0, X, Yc, Ac, eps=1, verbose=False):
                 # so accumulate any difference in the match loss
                 diff = np.sum((w - explored[i])**2)
                 match_loss += diff
+                match_count += 1
                 if verbose: print(f" {N}, {i} explored, diff = {diff}")
 
                 # and don't repeat any more work since this region was already explored
@@ -68,6 +92,7 @@ def graph_fitness(learning_rule, w0, X, Yc, Ac, eps=1, verbose=False):
             violation = np.fabs(np.minimum(dots - eps, 0)).max()
             # accumulate any violation in the region loss
             region_loss += violation
+            region_count += 1
             if verbose: print(f" {N}, {i} new, violation = {violation}")
 
             # use learning rule to push new weights for adjacent regions into the queue
@@ -80,22 +105,12 @@ def graph_fitness(learning_rule, w0, X, Yc, Ac, eps=1, verbose=False):
         W[N] = np.array([explored[i] for i in range(len(explored))])
 
     # return the loss values and weights
-    return region_loss, match_loss, W
+    return region_loss / region_count, match_loss / match_count, W
 
 if __name__ == "__main__":
 
-    import pickle as pk
-
     Ns = [3, 4, 5]
-    Yc, W, X, Ac = {}, {}, {}, {}
-
-    for N in Ns:
-
-        # load canonical hemis
-        ltms = np.load(f"ltms_{N}_c.npz")
-        Yc[N], W[N], X[N] = ltms["Y"], ltms["W"], ltms["X"]
-
-        with open(f"adjs_{N}_jc.npz", "rb") as f: Ac[N] = pk.load(f)
+    Yc, W, X, Ac = load_ltm_data(Ns)
 
     def perceptron_rule(w, x, y):
         return w + (y - np.sign((w * x).sum())) * x
